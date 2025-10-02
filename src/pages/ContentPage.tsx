fe9ac5bb3c2be61d04a4d4130442a7885d61d733
Brett Bilon <brettbilon@Bretts-MacBook-Pro-188.local>
2025-10-02 06:07:07 -0600

    feat(aeo): patch ContentPage with JSON-LD injector + fix validator for ES modules

--- src/pages/ContentPage.tsx
+++ src/pages/ContentPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import JSONLDInjector from '@/components/JSONLDInjector';\nimport { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import Header from "@/components/Header";
@@ -106,7 +106,7 @@ const ContentPage = () => {
             {JSON.stringify(videoJsonLd)}
           </script>
         )}
-      </Helmet>
+      </Helmet>\n  <JSONLDInjector jsonld={pageData?.jsonld ?? null} />\n
 
       {pageData && (
         <>
